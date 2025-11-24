a1596b1a84c21817e34ec1999ddefb2bbc6b26ef
Scott Senkeresty <scott@tinylizard.com>
2025-11-23 17:23:25 -0800

    chore: Bump version to 0.3.0

--- reveal/__init__.py
+++ reveal/__init__.py
@@ -3,7 +3,7 @@
 A clean, simple tool for progressive code exploration.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 # Import base classes for external use
 from .base import FileAnalyzer, register, get_analyzer
